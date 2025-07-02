e7ed060439697f6b95ae72f8b7e2274e86c08284
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-07-02 16:27:32 +0530

    nav bar set

---
+++ app/home/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../../components/navbar"
+const page = () => {
+  return (
+    <div className='w-full flex  justify-center'>
+        <div className=' w-full max-w-[1400px]'>
+        <Navbar/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default page
--- public/index.js
+++ public/index.js
@@ -3,11 +3,16 @@ import GoogleIcon from "./icons/Google.svg"
 import FaceBookIcon from "./icons/Facebook.svg"
 import AppleIcon from "./icons/Vector.svg"
 import GoWhiteIcon from "./icons/gocampusWhite.svg"
-
+import MainTicket from "./images/mainticket.svg"
+import GoCampusLogo from "./icons/gonavIcon.svg"
+import ProfileIcon from "./icons/profileIcon.svg"
 export { 
     LoginImage ,
     GoogleIcon,
     FaceBookIcon,
     AppleIcon,    
-    GoWhiteIcon
+    GoWhiteIcon,
+    MainTicket,
+    GoCampusLogo,
+    ProfileIcon
 };
