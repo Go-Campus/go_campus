6214ca0e96dab166219c13a47b28503470e7a831
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-07-03 09:48:48 +0530

    catagory

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import Image from "next/image";
-import {MainTicketLeft, MainTicketRight, PartyImage, Barcode, Sport,AcadamicIcon,Career,Culture, Technology, entertaiment, Workshopes} from "../../public"
-import Ticket from "../../components/mainTicket"
-import TypeCard from "../../components/typeCard"
-
+import {
+  MainTicketLeft,
+  MainTicketRight,
+  PartyImage,
+  Barcode,
+  Sport,
+  AcadamicIcon,
+  Career,
+  Culture,
+  Technology,
+  entertaiment,
+  Workshopes,
+} from "../../public";
+import Ticket from "../../components/mainTicket";
+import TypeCard from "../../components/typeCard";
 
 const cards = [
-  { id: 1, img: AcadamicIcon, name: 'Academic' },
-  { id: 2, img: Technology, name: 'Technology' },
-  { id: 3, img: entertaiment, name: 'Entertainment' },
-  { id: 4, img: Career, name: 'Career' },
-  { id: 5, img: Culture, name: 'Culture' },
-  { id: 6, img: Sport, name: 'Sport' },
-  { id: 7, img: Workshopes, name: 'Workshops' },
+  { id: 1, img: AcadamicIcon, name: "Academic" },
+  { id: 2, img: Technology, name: "Technology" },
+  { id: 3, img: entertaiment, name: "Entertainment" },
+  { id: 4, img: Career, name: "Career" },
+  { id: 5, img: Culture, name: "Culture" },
+  { id: 6, img: Sport, name: "Sport" },
+  { id: 7, img: Workshopes, name: "Workshops" },
 ];
 
 const page = () => {
@@ -24,15 +35,20 @@ const page = () => {
 
         {/* first section */}
         <div className="w-full flex  justify-center p-4">
-        <Ticket/>
-
+          <Ticket />
         </div>
-        <div className="justify-between   py-5 border-b border-[#ADADAD40] items-center flex w-full">
-  {cards.map((card) => (
-    <TypeCard key={card.id}   name={card.name} Image={card.img} className="" />
-  ))}
-</div>
-
+        <div className="w-full py-5 border-b border-[#ADADAD40] px-4">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-4 justify-items-center">
+            {cards.map((card) => (
+              <TypeCard
+                key={card.id}
+                name={card.name}
+                Image={card.img}
+                className="w-full max-w-[120px]"
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/typeCard/index.jsx
+++ components/typeCard/index.jsx
@@ -6,21 +6,24 @@ const TypeCard = ({
   className = '',
   ...props
 }) => {
+  // Log component rendering for debugging
+  console.log('TypeCard component rendered:', { name: props.name, className });
+
   return (
     <div
-      className={`  justify-center items-center flex  border border-[#ADADAD40] rounded-[26px] p-5 ${className}`}
+      className={`justify-center items-center flex border border-[#ADADAD40] rounded-[26px] p-3 sm:p-4 md:p-5 ${className}`}
       {...props}
     >
       {ImageSrc && (
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center w-full'>
         <Image 
-          className=' object-contain' 
+          className='object-contain w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-24 lg:h-24' 
           src={ImageSrc}
-          alt="Type card image"
+          alt={`${props.name} icon`}
           width={96}
           height={96}
         />
-        <p className='flex justify-center items-center text-[16px]'>
+        <p className='flex justify-center items-center text-xs sm:text-sm md:text-base text-center mt-2'>
             {props.name}
         </p>
         </div>
